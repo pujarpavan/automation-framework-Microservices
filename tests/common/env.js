48245fd083fb213ed104b8a15ecabf861c2ccc0f
Pujarpavan <94072845+pujarpavan@users.noreply.github.com>
2024-08-12 18:30:11 +0530

    Update env.js

--- tests/common/env.js
+++ tests/common/env.js
@@ -1,7 +1,7 @@
 // env.js
 
 export const environment = {
-    baseUrl: process.env.BASE_URL || 'http://your-app-url.com',
+    baseUrl: process.env.BASE_URL || 'https://10.134.119.220/login',
     apiEndpoint: process.env.API_ENDPOINT || 'http://api.your-app-url.com',
     timeout: process.env.TIMEOUT || 30000 // Default timeout for all tests
 };
